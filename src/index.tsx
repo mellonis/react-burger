f99fa0a7a55bc3f9e15e3662891275c4cac13e5d
Ruslan Gilmullin <mellonis@yandex.ru>
2021-06-26 22:30:31 +0300

    Prepares for review
    - Moves common.css to ./index.css

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './components/App';
-import './styles/common.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
