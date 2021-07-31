198d229e7f8949d32e248b1af62896c2c8ad7388
Ruslan Gilmullin <mellonis@yandex.ru>
2021-07-31 07:22:23 +0300

    Adds lexemes for the forms

--- src/consts.ts
+++ src/consts.ts
@@ -12,6 +12,7 @@ export const orderStatusToStatusTitleMap: {
 const allLexemes = {
   ru: {
     assembleABurger: 'Соберите бургер',
+    bottom: 'низ',
     buns: 'Булки',
     burgerFillings: 'Начинки',
     calories: 'Калории, ккал',
@@ -21,12 +22,48 @@ const allLexemes = {
     ingredientDetails: 'Детали ингредиента',
     orderId: 'Идентификатор заказа',
     orderList: 'Лента заказов',
-    sauces: 'Соусы',
     placeAnOrder: 'Оформиль заказ',
     profile: 'Личный кабинет',
     proteins: 'Белки, г',
+    sauces: 'Соусы',
     top: 'верх',
-    bottom: 'низ',
+    forms: {
+      __common__: {
+        doHidePassword: 'Скрыть пароль',
+        doLogin: 'Войти',
+        doRegister: 'Зарегистрироваться',
+        doShowPassword: 'Показать пароль',
+        email: 'E-mail',
+        haveYouRememberedYourPassword: 'Вспомнили пароль?',
+        name: 'Имя',
+        password: 'Пароль',
+      },
+      forgotPassword: {
+        doResetPassword: 'Восстановить',
+        provideYourEmail: 'Укажите e-mail',
+        title: 'Восстановление пароля',
+      },
+      login: {
+        areYouForgetYourPassword: 'Забыли пароль?',
+        areYouTheNewUser: 'Вы новый пользователь?',
+        doResetYourPassword: 'Восстановить пароль',
+        title: 'Вход',
+      },
+      profile: {
+        doEdit: 'Изменить',
+        email: 'Логин',
+      },
+      register: {
+        areYouRegisteredAlready: 'Уже зарегистрированы?',
+        title: 'Регистрация',
+      },
+      resetPassword: {
+        doReset: 'Сохранить',
+        provideApprovalCode: 'Введите код из письма',
+        provideYourNewPassword: 'Введите новый пароль',
+        title: 'Восстановление пароля',
+      },
+    },
   },
 };
 
