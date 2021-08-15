4fd931919a2c372139620326149b0a077d383b7d
Ruslan Gilmullin <mellonis@yandex.ru>
2021-08-15 03:07:56 +0300

    Refactors
    - Updates typings

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { useDispatch, useSelector } from 'react-redux';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { burgerReducer, userReducer } from './reducers';
 
 const store = configureStore({
@@ -14,7 +14,9 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector = <T>(func: (state: RootState) => T): T =>
-  useSelector(func);
+export const useAppSelector = <T>(
+  func: (state: RootState) => T,
+  cmp?: typeof shallowEqual
+): T => useSelector(func, cmp);
 
 export { store };
